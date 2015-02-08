e488c5e22257032eee297d3afee6a9801a14f26b
Breezewish <me@breeswish.org>
2015-02-08 17:48:04 +0800

    [app] implement role & permission system

---
+++ src/User/Role.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * This file is part of openvj project.
+ *
+ * Copyright 2013-2015 openvj dev team.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace VJ\User;
+
+use VJ\Core\Application;
+
+class Role
+{
+    private $roles;
+
+    /**
+     * @param int|null $uid
+     */
+    public function __construct($uid = null)
+    {
+        if ($uid === null) {
+            $this->roles = [];
+            return;
+        }
+
+        $data = Application::coll('UserRole')->findOne([
+            'uid' => (int)$uid
+        ]);
+
+        if ($data === null) {
+            $this->roles = [];
+            return;
+        }
+
+        $this->roles = $data['d'];
+    }
+
+    /**
+     * 得到该用户所有角色
+     *
+     * @param \MongoId $domainId
+     * @param bool $isResourceOwner
+     * @return array
+     */
+    public function getRoles(\MongoId $domainId, $isResourceOwner = false)
+    {
+        $domainId = (string)$domainId;
+        if (!isset($this->roles[$domainId])) {
+            $roles = [];
+        } else {
+            $roles = $this->roles[$domainId];
+        }
+        $roles[] = 'EVERYONE';
+        if ($isResourceOwner) {
+            $roles[] = 'OWNER';
+        }
+
+        return $roles;
+    }
+
+    /**
+     * 是否拥有一项权限（白名单）
+     *
+     * @param string $permissionName
+     * @param \MongoId $domainId
+     * @param bool $isResourceOwner
+     * @return bool
+     */
+    public function hasPermission($permissionName, \MongoId $domainId, $isResourceOwner = false)
+    {
+        $roles = $this->getRoles($domainId, $isResourceOwner);
+
+        $result = Application::coll('PermissionAllow')->findOne([
+            'domain' => $domainId,
+            'val' => $permissionName,
+            'role' => ['$in' => $roles],
+        ]);
+
+        return ($result !== null);
+    }
+}
---
+++ src/User/RoleManager.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * This file is part of openvj project.
+ *
+ * Copyright 2013-2015 openvj dev team.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace VJ\User;
+
+use VJ\Core\Application;
+
+class RoleManager
+{
+    private static $overWriteUid = null;
+
+    /**
+     * 在函数调用期间使用指定的 uid 作为权限控制主体的标识符
+     *
+     * @param int $uid
+     * @param callable $callback
+     */
+    public static function overWriteToken($uid, callable $callback)
+    {
+        $lastOverWrite = self::$overWriteUid;
+        self::$overWriteUid = $uid;
+        $callback();
+        self::$overWriteUid = $lastOverWrite;
+    }
+
+    /**
+     * 创建一个角色
+     *
+     * @param string $name
+     * @param bool $internal
+     * @param \MongoId|null $domain
+     * @param int|null $owner
+     * @return bool
+     */
+    public static function createRole($name, $internal = false, \MongoId $domain = null, $owner = null)
+    {
+        if (!$internal) {
+            if ($domain === null || $owner === null || !$domain instanceof \MongoId) {
+                throw new \InvalidArgumentException('required domain and owner');
+            }
+            if (strlen($name) <= 2 || substr($name, 0, 2) !== '$$') {
+                throw new \InvalidArgumentException('name should starts with "$$"');
+            }
+        }
+
+        $name = strtoupper($name);
+
+        if (!$internal) {
+            $result = Application::coll('Role')->update([
+                'domain' => $domain,
+                'name' => $name,
+            ], [
+                '$setOnInsert' => [
+                    'owner' => (int)$owner,
+                    'at' => new \MongoDate()
+                ]
+            ], [
+                'upsert' => true
+            ]);
+        } else {
+            $result = Application::coll('Role')->update([
+                'internal' => true,
+                'name' => $name,
+            ], [], [
+                'upsert' => true
+            ]);
+        }
+
+        return ($result['n'] === 1);
+    }
+
+    /**
+     * 获得一个域下所有角色
+     *
+     * @param \MongoId $domain
+     * @return array
+     */
+    public static function getDomainRoles(\MongoId $domain)
+    {
+        $cursor = Application::coll('Role')->find([
+            '$or' => [
+                ['internal' => true],
+                ['domain' => $domain]
+            ]
+        ]);
+
+        return iterator_to_array($cursor, false);
+    }
+}
