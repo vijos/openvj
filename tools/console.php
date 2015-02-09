14799e8a75115e95a53b77e8a371034c9f3cf00d
Breezewish <me@breeswish.org>
2015-02-09 17:24:02 +0800

    [app] missing copyright

--- tools/console.php
+++ tools/console.php
@@ -1,5 +1,13 @@
 #!/usr/bin/env php
 <?php
+/**
+ * This file is part of openvj project.
+ *
+ * Copyright 2013-2015 openvj dev team.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 const MODE_TEST = false;
 $loader = require __DIR__ . '/../vendor/autoload.php';
