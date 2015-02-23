485610fe932e5e3fe11635e2cf3ec1e5cbdb2b47
Breezewish <me@breeswish.org>
2015-02-23 11:40:34 +0800

    [frontend] initial commit

---
+++ web/js/require-config.js
@@ -0,0 +1,18 @@
+require.config({
+  shim: {
+
+  },
+  paths: {
+    ace: "../bower_components/ace/lib/ace",
+    foundation: "../bower_components/foundation/js/foundation",
+    "google-code-prettify": "../bower_components/google-code-prettify/bin/prettify.min",
+    jquery: "../bower_components/jquery/dist/jquery",
+    marked: "../bower_components/marked/lib/marked",
+    requirejs: "../bower_components/requirejs/require",
+    twig: "../bower_components/twig.js/twig.min",
+    underscore: "../bower_components/underscore/underscore"
+  },
+  packages: [
+
+  ]
+});
